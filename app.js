2172ed7f55fff92a876f849dee92aed2ffbbc3d0
jgctron <116297714+jgctron@users.noreply.github.com>
2024-08-25 14:00:44 -0400

    Initial commit: Added all project files

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve the HTML file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
