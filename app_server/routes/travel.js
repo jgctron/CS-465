2d80fadb3a9e089771db83f8375007f79f59cf60
Jorge Cintron <116297714+jgctron@users.noreply.github.com>
2024-09-17 14:03:36 -0400

    Delete app_server/routes/travel.js

--- app_server/routes/travel.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const travelController = require('../controllers/travelController');
-
-router.get('/travel', travelController.getTravelPage);
-
-module.exports = router;
