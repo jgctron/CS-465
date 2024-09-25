41da84b360326fc75ce1d192c0e2b8cccc9801b3
Jorge Cintron <116297714+jgctron@users.noreply.github.com>
2024-09-25 16:52:00 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-var express = require('express');
-var path = require('path');
-var hbs = require('hbs');  // Handlebars view engine
+// Import necessary packages
+const express = require('express');
+const path = require('path');
+const hbs = require('hbs');  // Handlebars view engine
+const db = require('./app_server/db');  // MongoDB connection (make sure db.js is set up)
+const Trip = require('./app_server/models/travlr'); // Import the Trip model
 
-var app = express();
+const app = express();
 
 // Set up Handlebars as the view engine
 app.set('views', path.join(__dirname, 'app_server', 'views'));
@@ -15,8 +18,33 @@ hbs.registerPartials(path.join(__dirname, 'app_server', 'views', 'partials'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Use the routes defined in the routes/index.js file
-var indexRouter = require('./app_server/routes/index');
+const indexRouter = require('./app_server/routes/index');
 app.use('/', indexRouter);
+
+// Route to display all trips
+app.get('/trips', (req, res, next) => {
+    Trip.find({}, (err, trips) => {
+        if (err) {
+            return next(err);
+        }
+        res.render('trips', { trips });  // 'trips' is the Handlebars view
+    });
+});
+
+// Middleware for error handling (optional)
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: err
+    });
+});
 
 // Start the server on port 3000
 app.listen(3000, function () {
